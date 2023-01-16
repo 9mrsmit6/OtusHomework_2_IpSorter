339b9c89aca268ea4333b88c96b0fbda0addb746
Grigoriy <9mrsmit6@gmail..com>
2023-01-16 07:37:57 +0300

    немного пигррался с исходниками

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <utility>
+
+
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -10,12 +14,15 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
+
+
+auto split(const std::string &str, char d)
 {
     std::vector<std::string> r;
 
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
+    auto stop = str.find_first_of(d);
+    decltype(stop) start = 0;
+
     while(stop != std::string::npos)
     {
         r.push_back(str.substr(start, stop - start));
@@ -35,7 +42,15 @@ int main()
       {
           std::vector<std::vector<std::string> > ip_pool;
 
-          for(std::string line; std::getline(std::cin, line);)
+          std::ifstream file("ip_filter.tsv");
+          if(!file.is_open())
+          {
+              return 1;
+          }
+          std::cout<<"FileOpened"<<std::endl;
+
+
+          for(std::string line; std::getline(file, line);)
           {
               std::vector<std::string> v = split(line, '\t');
               ip_pool.push_back(split(v.at(0), '.'));
